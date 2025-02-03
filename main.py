cf21181e0e40ea750a64b3bef08361fc169e622c
Tiago Silva <168717992+TokiSyt@users.noreply.github.com>
2025-02-03 22:19:44 +0100

    ProjectUpdates
    Includes better error handling and more readability.

--- main.py
+++ main.py
@@ -1,81 +1,79 @@
-def organization(texto, user_input):
-
-	alphabet = {
-		"a": "a",
-		"b": "b",
-		"c": "c",
-		"d": "d",
-		"e": "e",
-		"f": "f",
-		"g": "g",
-		"h": "h",
-		"i": "i",
-		"j": "j",
-		"k": "k",
-		"l": "l",
-		"m": "m",
-		"n": "n",
-		"o": "o",
-		"p": "p",
-		"q": "q",
-		"r": "r",
-		"s": "s",
-		"t": "t",
-		"u": "u",
-		"v": "v",
-		"w": "w",
-		"x": "x",
-		"y": "y",
-		"z": "z"
+alphabet = {
+	"a": "a",
+	"b": "b",
+	"c": "c",
+	"d": "d",
+	"e": "e",
+	"f": "f",
+	"g": "g",
+	"h": "h",
+	"i": "i",
+	"j": "j",
+	"k": "k",
+	"l": "l",
+	"m": "m",
+	"n": "n",
+	"o": "o",
+	"p": "p",
+	"q": "q",
+	"r": "r",
+	"s": "s",
+	"t": "t",
+	"u": "u",
+	"v": "v",
+	"w": "w",
+	"x": "x",
+	"y": "y",
+	"z": "z"
 	}
 
-	texto_compl = ""
-	alpha_check = user_input.isalpha()
-	lowered_input = user_input.lower()
-	
 
-	if lowered_input == "all":
+def organization(text, user_input):
+	if user_input == "all":
 
-		contador_letras = letters_counted(texto)
-		sorted_letras = sorted(contador_letras.keys())
+		total_letters = letters_counted(text)
+		sorted_letters = dict(sorted(total_letters.items()))
 
-		for letra in sorted_letras:
-			numero = contador_letras.get(letra)
-			frase = "The letter {} appears {} times.".format(letra, numero)
-			texto_compl += frase + "\n"
-		print(texto_compl)
-	
-	elif lowered_input == "none":
-		pass
+		for letter in sorted_letters:
+			letter_amount = sorted_letters.get(letter)
+			if letter == "z":
+				print(f"The letter {letter} appears {letter_amount} times.")
+			else:
+				print(f"The letter {letter} appears {letter_amount} times.\n")
 
-	elif alpha_check:
-		if len(lowered_input) == 1:
-			if lowered_input in alphabet:
-				l_counted = letters_counted(texto)
-				number = l_counted.get(alphabet[lowered_input])
-				print(f"The letter {lowered_input} appears {number} times.\n")
-
-	else:
-		raise Exception("Invalid input. Please select one of the options above")
+	elif user_input in alphabet:
+			l_counted = letters_counted(text)
+			number = l_counted.get(alphabet[user_input])
+			print(f"The letter {user_input} appears {number} times.")
 
 
-def main():
-	book_path = "BookBot/bookbot/Shrek.txt"
-	livro = get_book_text(book_path)
-	checker_palavras = input("Do you wish to see how many words this script contain in total? (y/n)\n")
-	checker_letras = input('''Letters counter:
-	None - No letters.
+def words_option():
+	while True:
+		try:
+			checker_words = input("Do you wish to see how many words this script contain in total? (y/n)\n")
+			if checker_words in ["y", "n"]:
+				return checker_words
+			else:
+				print('Please enter "y" or "n".')
+		except:
+			print("Enter a valid answer.")
+
+
+def letters_option():
+	while True:
+		try:
+			checker_letters = input('''\nRegarding a letters count, how my letters do you wish to see:
+	Zero - No letters.
 	All - All letters.
 	Single letter from A to Z - That letter count.
 						
 	Answer here: ''')
-	print("\n--- Begin report of Shrek Script ---\n")
-
-	palavras_contadas(livro, checker_palavras)
-	organization(livro, checker_letras)
-
-
-	print("\n--- End report ---")
+			if checker_letters.lower() in alphabet and len(checker_letters) == 1 or checker_letters.lower() in ["zero", "all"]:
+				return checker_letters.lower()
+			else:
+				print('Please enter Zero, All or a single letter from the alphabet.')
+		except:
+			print("Enter a valid answer from the options below.")
 
 
 def get_book_text(path):
@@ -83,26 +81,45 @@ def get_book_text(path):
 		return f.read()
 
 
-def palavras_contadas(texto, user_input):
-	if user_input == "n":
-		return None
-	elif user_input == "y":
-		numero = len(texto.split())
-		print(f"{numero} words found in the document\n")
-	else:
-		raise Exception("Invalid answer! Choose yes or no.")
+def main():
+	book_path = "Shrek.txt"
+	book = get_book_text(book_path)
+	checker_words = words_option()
+	checker_letters = letters_option()
+
+	print("\n--- Begin report of Shrek Script ---\n")
+
+	if checker_words == "y":
+		counted_words(book)
+	elif checker_words == "n" and checker_letters != "zero":
+		print("No words information to print selected.\n")
+	
+	if checker_letters != "zero":
+		organization(book, checker_letters)
+	elif checker_letters == "zero" and checker_words == "y":
+		print("No letters information to print selected.")
+
+	if checker_words == "n" and checker_letters == "zero":
+		print("No book informations to print selected.")
+
+	print("\n--- End report ---")
 
 
-def letters_counted(texto):
-	lowered_string = texto.lower()
-	contador_letras = {}
+def counted_words(text):
+	words_amount = len(text.split())
+	print(f"{words_amount} words found in the selected document.\n")
+
+
+def letters_counted(text):
+	lowered_string = text.lower()
+	letters_counter = {}
 	for char in lowered_string:
 		if char.isalpha():
-			if char in contador_letras:
-				contador_letras[char] += 1
+			if char in letters_counter:
+				letters_counter[char] += 1
 			else:
-				contador_letras[char] = 1
-	return contador_letras
+				letters_counter[char] = 1
+	return letters_counter
 
 
 main()
